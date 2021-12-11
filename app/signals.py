409dada4eb7512b9fe67ab8c61dda124f4a1c683
Richard Guy <rich.guy@cantab.net>
2021-12-11 10:50:44 +0000

    live stream video to remote clients using WebRTC

--- app/signals.py
+++ app/signals.py
@@ -10,15 +10,30 @@ from flask_socketio import emit, send, join_room
 
 from app import socketio
 
-ROOM = 'room'
+broadcaster = None
 
-@socketio.on('connect')
-def connect():
-    print('Client connected')
-    emit('ready', to=ROOM)
-    join_room(ROOM)
+@socketio.on('broadcaster')
+def on_broadcaster():
+    global broadcaster
+    broadcaster = request.sid
+    emit('broadcaster', broadcast=True)
 
-@socketio.on('message')
-def message(m):
-    print(m)
-    emit('message', m, to=ROOM)
+@socketio.on('watcher')
+def on_watcher():
+    emit('watcher', request.sid, to=broadcaster)
+
+@socketio.on('disconnect')
+def on_disconnect():
+    emit('disconnectPeer', request.sid, to=broadcaster)
+
+@socketio.on('offer')
+def on_offer(id, message):
+    emit('offer', (request.sid, message), to=id)
+
+@socketio.on('answer')
+def on_offer(id, message):
+    emit('answer', (request.sid, message), to=id)
+
+@socketio.on('candidate')
+def on_offer(id, message):
+    emit('candidate', (request.sid, message), to=id)
